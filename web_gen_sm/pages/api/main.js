c796e59c2e26800056966a363f0b6a02a3b98900
Atamarado <ginescarreto@gmail.com>
2022-04-08 17:56:27 +0200

    Updated today's work in liveshare

--- web_gen_sm/pages/api/main.js
+++ web_gen_sm/pages/api/main.js
@@ -1,5 +1,34 @@
-export async function createUser() {
+// https://javascript.plainenglish.io/connect-mysql-and-authentication-on-next-js-761d12340e4f
 
+import crypto from 'crypto';
+import { v4 as uuidv4 } from 'uuid';
+import excuteQuery from 'db.js';
+import moment from 'moment';
+
+export async function createUser(nombre, apellido_uno, apellido_dos, fecha_nacimiento,
+    alias, correo, password) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto
+        .pbkdf2Sync(password, salt, 1000, 64, 'sha512')
+        .toString('hex');
+    const user = {
+        id: uuidv4(),
+        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        email,
+        hash,
+        salt,
+    };
+    try {
+        const result = await excuteQuery({
+            query: 'INSERT INTO usuario (nombre,apellido_1,apellido_2,mail,password,fecha_nacim,username) VALUES (?,?,?,?,?,?,?)',
+            values: [user.id, user.createdAt.toString(), user.email, user.hash, user.salt],
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return user;
 }
 
 export async function autenticateUser() {
@@ -15,12 +44,19 @@ export async function emotionLogout() {
 }
 
 export async function getEmotions() {
+    try {
+
+        const result = await excuteQuery({
+            query: 'SELECT imagen FROM emocion',
+            values: [],
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
-export async function getMotivationalPhrase() {
-
-}
 
 export async function getEvents() {
 
@@ -38,7 +74,17 @@ export async function createCalendarEvent() {
 
 }
 
-export async function createPost() {
+export async function createPost(premium, fechapost, idUsuario, idEmocion, path_img, descripcion) {
+
+    try {
+        const result = await excuteQuery({
+            query: 'INSERT INTO post (premium,fechapost,idUsuario,idEmocion,path_img,descripcion) VALUES (?,?,?,?,?,?)',
+            values: [premium, fechapost, idUsuario, idEmocion, path_img, descripcion],
+        });
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
@@ -46,15 +92,32 @@ export async function deletePost() {
 
 }
 
-export async function getPhrase() {
+export async function getMotivationalPhrase(idFrase) {
+
+    try {
+
+        const result = await excuteQuery({
+            query: 'SELECT frase FROM frase WHERE id = ?',
+            values: [idFrase]
+        });
+        console.log(result);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
 export async function isAuthor(idUsuario, idPost) {
 
     try {
-        const result:
+        const result = await executeQuery({
+            query: `SELECT * FROM posts WHERE post.idPost=? post.idUsuario=?`,
+            values: [idPost, idUsuario]
+        })
 
+        console.log(result);
+        //return [0];
     } catch (error) {
         console.log(error);
     }
