b0c58390eab5f0ee4e7af64f0d2bb0543db957e0
Atamarado <ginescarreto@gmail.com>
2022-04-08 17:22:40 +0200

    Main.js creado, con las funciones necesarias

---
+++ web_gen_sm/pages/api/main.js
@@ -0,0 +1,55 @@
+function createUser() {
+
+}
+
+function autenticateUser() {
+
+}
+
+function emotionLogin() {
+
+}
+
+function emotionLogout() {
+
+}
+
+function getEmotions() {
+
+}
+
+function getMotivationalPhrase() {
+
+}
+
+function getEvents() {
+
+}
+
+function postReaction() {
+
+}
+
+function getCalendarEvent() {
+
+}
+
+function createCalendarEvent() {
+
+}
+
+function createPost() {
+
+}
+
+function deletePost() {
+
+}
+
+function getPhrase() {
+
+}
+
+function isAuthor() {
+
+}
