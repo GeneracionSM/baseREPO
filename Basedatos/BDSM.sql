97e1442711f08824158723906fb8fa01a10a4f3b
Álvaro Pimentel <alvaropimentel124@gmail.com>
2022-04-08 13:47:32 +0200

    Update BDSM.sql

--- Basedatos/BDSM.sql
+++ Basedatos/BDSM.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-04-2022 a las 13:43:57
+-- Tiempo de generación: 08-04-2022 a las 13:46:43
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
