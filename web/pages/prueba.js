5ebce1ca8c9090d74100d2582b8a7279c4f00376
Sergi Mayol <sergi2808@gmail.com>
2022-04-09 14:13:27 +0200

    enlace db actualizado

--- web/pages/prueba.js
+++ web/pages/prueba.js
@@ -1,4 +1,8 @@
 import sql_query from "../../lib/db";
+import getMotivationalPhrase from "./api/querries";
+import getUser from "./api/querries";
+import createUser from "./api/querries";
+import executeQuery from "../../lib/db"
 
 export default function hackathon(props) {
     const { posts } = props;
@@ -7,11 +11,12 @@ export default function hackathon(props) {
     return <div>
         <h1>Welcome to the blog page.....</h1>
         {
+            //Aqui va el front end :D
             // render posts here
             posts.map(
                 (item) => <>
-                    <h2>{item.title}</h2>
-                    <p>{item.content}</p>
+                    <h2>{item.nombre}</h2>
+                    <p>{item.imagen}</p>
                     <br />
                 </>
             )
@@ -20,21 +25,20 @@ export default function hackathon(props) {
     </div>
 }
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
     // fecth endpoint
     try {
-        const result = await sql_query(
-        `
-          SELECT * FROM emocion
-        `
-        );
-        let posts = JSON.parse(JSON.stringify(result));
-        console.log(posts);
+        //switch ()
+        /*const result = await executeQuery(`
+        SELECT * FROM usuario
+        `);*/
+        const result = await getUser();
+        let posts = JSON.parse(JSON.stringify(result))
         return {
             props: { posts } // will be passed to our blog page component as props
         };
     } catch (e) {
-        console.log(e)
+        console.log(e);
         return { props: { posts: false } }
     }
 }
